2addda19479ee09d6632c28ce201b626ef08a1eb
busesorin94 <sorin-marian.buse@my.fmi.unibuc.ro>
2016-01-16 18:27:52 +0200

    'Styling'

--- myApp/forms.py
+++ myApp/forms.py
@@ -3,8 +3,9 @@ from django.forms import Form, CharField, Textarea
 
 class MovieCommentForm(Form):
     comment_text = CharField(widget=Textarea(
-        attrs={'cols': 50, 'rows': 4, 'placeholder': 'Write a comment...'}),
+        attrs={'cols': 50, 'rows': 4, 'placeholder': 'Write a comment...', 'class': 'form-control'}),
         label='')
 
+
 class SearchMoviesForm(Form):
-    search = CharField(label="Search movies:")
+    search = CharField(label="Search movies:", widget=Textarea(attrs={'cols': 20, 'rows': 1, 'class': 'form-control'}))
