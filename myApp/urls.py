653b90421b7c42a3015d818497032165ec1de697
petreflorina <petreflorinamaria@gmail.com>
2016-01-16 14:11:34 +0200

    added Watchlist for one user

--- myApp/urls.py
+++ myApp/urls.py
@@ -3,4 +3,6 @@ from myApp import views
 
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
-        url(r'^search/$', views.search, name='search'))
+        url(r'^search/$', views.search, name='search'),
+        url(r'^simulate/(?P<pk>[0-9]+)$', views.SimulateWatchlist.as_view(), name='AddToWatchlist'),
+        url(r'^allmovieseen/$', views.AllMoviesSeen.as_view(), name='MoviesSeen'))
--- myApp/views.py
+++ myApp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 from myApp.forms import MovieCommentForm
+from  django.views.generic.base import TemplateView
+from myApp.models import Movie, WatchList
 
 
 def index(request):
@@ -22,3 +24,45 @@ def index(request):
 def search(request):
     context_dict = {'boldmessage': "I am bold font from the context"}
     return render(request, 'search.html', context_dict)
+
+class SimulateWatchlist(TemplateView):
+    template_name = 'index.html'
+    
+    def get(self, request, pk):
+        movie = Movie.objects.get(pk=pk)
+        user = request.user
+
+        if WatchList.objects.filter(movie=movie, user=user).count() > 0:
+            message = "Movie already in list!"
+        else:
+            watchlist = WatchList(movie=movie, user=request.user)
+            watchlist.save()
+            message = "Movie added to watchlist"
+        
+        context_dict = {
+            'boldmessage': message 
+        }
+
+        return render(request, self.template_name, context_dict)
+
+
+class AllMoviesSeen(TemplateView):
+    template_name = 'index.html'
+
+    def get(self, request):
+
+        user=request.user
+
+        movies=WatchList.objects.get(user=user)
+
+        movieSeen = movies.movie
+        context_dict = {
+            'nume' : movieSeen.name,
+            'genre': movieSeen.genre,
+            'duration' : movieSeen.duration
+        }
+
+        return render(request, self.template_name, context_dict)
+
+       
+        
